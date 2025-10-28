b95b1bb4827a2304f04954095924b307ae3c206c
tkv4uk <annatachuk54@gmail.com>
2025-10-28 20:45:37 +0300

    Новая ветка и изменение в игре

--- main.py
+++ main.py
@@ -5,13 +5,14 @@ def rock_paper_scissors():
     choices = ["камень", "ножницы", "бумага"]
 
     print("Игра 'ЦУ-Е-ФА'")
+    print("Давай узнаем, кто сильнее!")
     print("Выбери: камень, ножницы или бумага")
 
     while True:
         user_choice = input("Твой выбор (или 'выход' для завершения): ").lower()
 
         if user_choice == "выход":
-            print("Спасибо за игру!")
+            print("Спасибо за игру! Возвращайся!")
             break
 
         if user_choice not in choices:
@@ -22,12 +23,13 @@ def rock_paper_scissors():
         print(f"Компьютер выбрал: {computer_choice}")
 
         if user_choice == computer_choice:
-            print("Ничья!")
+            print("Ничья! Сыграем еще раз?")
         elif (user_choice == "камень" and computer_choice == "ножницы") or \
                 (user_choice == "ножницы" and computer_choice == "бумага") or \
                 (user_choice == "бумага" and computer_choice == "камень"):
-            print("Ты выиграл!")
+            print("Поздравляю! Ты выиграл!")
         else:
-            print("Ты проиграл!")
+            print("Увы, на этот раз ты проиграл!")
+
 
 rock_paper_scissors()
